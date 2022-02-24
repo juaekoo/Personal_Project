441a16dc1aa784875020ccc6ee2e4ba0e29e831a
juaekoo <jool0129@gmail.com>
2022-02-25 02:26:08 +0900

    .

--- upcomingData.js
+++ upcomingData.js
@@ -1,10 +1,24 @@
 const upcomingData = [
     {
-    no: 1,
-    img: 'images/Uncharted.jpg',
-    date: new Date('2022-1'),
-    name: '언차티드', 
-    service: 'boxoffice'
+    no: 4,
+    img: 'images/Spencer.jpg',
+    date: '2022-03-16',
+    name: '스펜서', 
+    service: 'Box office'
+    },
+    {
+    no: 5,
+    img: 'images/Jurassic.jpg',
+    date: '2022-06',
+    name: '쥬라기 월드: 도미니언', 
+    service: 'Box office'
+    },
+    {
+    no: 6,
+    img: 'images/AVATAR2.jpg',
+    date: '2023',
+    name: '아바타2', 
+    service: 'Box office'
     }
 ]
 
--- upcomingDataList.js
+++ upcomingDataList.js
@@ -10,6 +10,7 @@ upcomingData.forEach( (item, index) => {
         const value = item[keyName];
 
         const div = document.createElement('div');
+        const span = document.createElement('span');
         const text = document.createTextNode(value);
 
         if (keyName === 'img') {
@@ -21,15 +22,36 @@ upcomingData.forEach( (item, index) => {
             div.appendChild(link);
             div.setAttribute('class', 'img-wrapper');
             moviediv.appendChild(div);
-        } else if (keyName === 'name' || keyName === 'star') {
+
+        } else if (keyName === 'date') {
+            if (value.length == 4) {
+                const movieyear = new Date(value).getFullYear();
+                const movieDay = movieyear + '년 '
+                date = document.createTextNode(movieDay);
+            } else if ( value.length == 7 ) {
+                const moviemonth = new Date(value).getMonth() + 1;
+                const movieDay = moviemonth + '월 '
+                date = document.createTextNode(movieDay);
+            } else {
+                const moviemonth = new Date(value).getMonth() + 1;
+                const moviedate = new Date(value).getDate();
+                const movieDay = moviemonth + '월 ' + moviedate + '일 '
+                date = document.createTextNode(movieDay);
+            }
+            span.appendChild(date);
+            span.setAttribute('class', 'date');  
+            moviediv.appendChild(span);      
+
+        } else if(keyName === 'name') {
             const link = document.createElement('a');
             link.setAttribute('href', './viewUpcomingMovie.html?no=' + item['no']);
             link.appendChild(text);
-            div.appendChild(link);
+            span.appendChild(link);
+            span.setAttribute('class', 'name');  
+            moviediv.appendChild(span);
         }
-
-        moviediv.appendChild(div);
 
         upcomings.appendChild(moviediv);
     }
+    
 })
