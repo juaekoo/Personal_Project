07229b4371e988d1436d946754158fa727b6946a
juaekoo <jool0129@gmail.com>
2022-02-24 11:30:01 +0900

    .

--- collectionData.js
+++ collectionData.js
@@ -1,17 +1,23 @@
 const collectionData = [
     {
     title: '눈 오는날의 영화들',
-    movie: '헤이트풀8, 러브레터, 나홀로집에',
+    movie1: '헤이트풀8',
+    movie2: '러브레터', 
+    movie3: '나홀로집에',
     summary: '눈오는 배경을 가진 영화들이에요!'
     },
     {
     title: '잔잔한 듯 흥미진진',
-    movies: '쇼생크 탈출, 기생충, 트루먼쇼, 캐치미이프유캔',
+    movie1: '캐치미이프유캔',
+    movie2: '기생충', 
+    movie3: '트루먼쇼',
     summary: ''
     },
     {
     title: '하마터면 눈물흘릴뻔...',
-    movies: '소울, 업',
+    movie1: '소울', 
+    movie2: '업',
+    movie3: '',
     summary: '휴지 한 장 준비하세요ㅠㅠ'
     }
 ]
