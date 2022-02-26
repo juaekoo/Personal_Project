237dc181b078e2755d9c6c33bea7f5eb4ccd7176
juaekoo <jool0129@gmail.com>
2022-02-26 23:42:00 +0900

    login validation complete

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#submit').click(function(){
+    $('#loginBtn').click(function(){
 
         // 각 사이트의 아이디 규칙
         // 네이버: 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능 (반드시 첫문자는 영/숫자만 가능.)
@@ -9,25 +9,25 @@ $(document).ready(function(){
         
         // 위를 참고로 일단 아이디란에 영문대소문자, 숫자, 특수기호(_-.') 모두 입력은 가능하게 함 
         // 자세한 아이디규칙은 여기 로그인단계에서는 적용X. 어짜피 DB에서 회원정보랑 비교해서 체크할 것이기 때문
-        const emailFormat = RegExp(/^[A-Za-z0-9_\-\'\.]+.@[A-Za-z0-9]+\.[A-Za-z0-9]+$/);
-        const passwordFormat = RegExp(/^[A-Za-z0-9]{8,}$/);
+        var emailFormat = RegExp(/^[A-Za-z0-9_\-\'\.]+.@[A-Za-z0-9]+\.[A-Za-z0-9]+$/);
+        var passwordFormat = RegExp(/^[A-Za-z0-9]{8,}$/);
 
         var email = $('input[name="email"]').val();
         var password = $('input[name="password"]').val();
         if(!email) {
             $('input[name="email"]').css('border', 'solid 1px red');
-            $('input[name="email"]').css('box-shado', '0px 0px 5px 3px red');
-        } else if(!getCheck.emailFormat(email)){
+            $('input[name="email"]').css('box-shadow', '0px 0px 5px 3px firebrick');
+        } else if(!emailFormat.test(email)){
             $('#alarm').html('이메일 형식이 올바르지 않습니다.')
         }
         if(!password) {
             $('input[name="password"]').css('border', 'solid 1px red');
-            $('input[name="password"]').css('box-shado', '0px 0px 5px 3px red');
-        } else if(!getCheck.passwordFormat(password)){
+            $('input[name="password"]').css('box-shadow', '0px 0px 5px 3px firebrick');
+        } else if(!passwordFormat.test(password)){
             $('#alarm').html('비밀번호 형식이 올바르지 않습니다.')
         }
 
-        if(getCheck.emailFormat(email) && getCheck.passwordFormat(password)) {
+        if(emailFormat.test(email) && passwordFormat.test(password)) {
             location.assign('home.html');
         }
     })
